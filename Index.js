08db77be2c7eb42dff47983ef0a2838a21f43f20
Dilshad <hmydilshadkt@gmail.com>
2024-01-31 18:46:18 +0530

    fix: changed env

--- Index.js
+++ Index.js
@@ -2,31 +2,34 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const PORT = process.env.PORT;
+const PORT = 8000;
 const user = require("./Models/userSchema");
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const userRouter = require("./Routes/userRoutes");
 
-mongoose.connect(process.env.MONGO_URL).then(async () => {
-  console.log("database connected");
-  const admin = await user.findOne({ role: "admin" });
-  if (!admin) {
-    const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 12);
-    await user.create({
-      name: process.env.ADMIN_NAME,
-      email: process.env.ADMIN_EMAIL,
-      hashedPassword,
-      role: "admin",
-    });
-  }
-});
+mongoose
+  .connect(
+    "mongodb+srv://dilshadbasith01:dilu123@cluster0.2f6uned.mongodb.net/stayHub?retryWrites=true&w=majority"
+  )
+  .then(async () => {
+    console.log("database connected");
+    const admin = await user.findOne({ role: "admin" });
+    if (!admin) {
+      const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 12);
+      await user.create({
+        name: process.env.ADMIN_NAME,
+        email: process.env.ADMIN_EMAIL,
+        hashedPassword,
+        role: "admin",
+      });
+    }
+  });
 
 const ErrorHandler = require("./Middlewares/ErrorHandler");
 const commonRouter = require("./Routes/commonRoutes");
 const authRoute = require("./Routes/authRoute");
 const adminRoute = require("./Routes/adminRoutes");
-
 
 const app = express();
 app.use(cors());
